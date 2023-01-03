3afa323de9b6dbe30a4e36b603cd14b8f776286b
Erick Roberto <erickkan2004@gmail.com>
2023-01-02 21:26:55 -0300

    main

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// TagName - Ver quantidade de Elementos - mostra em Array
+/*
+let h1 = document.getElementsByTagName('h1')
+
+console.log(h1[0])
+console.log(h1[1])
+
+
+
+//By Id - Pelo ID
+
+let div = document.getElementById('teste')
+
+console.log(div)
+
+//Class Name - Pela Class
+
+let button = document.getElementsByClassName('botao')
+
+console.log(button)
+
+
+
+let div = document.querySelectorAll('#teste ul li')
+
+console.log(div)
+
+*/
+
+function clicou(){
+    const teste = document.querySelector('#teste')
+    
+    const ul = teste.querySelector('ul')
+    
+}
+
