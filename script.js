153c168069ad4a4b05e4cc1ff6c967da640209df
Erick Roberto <erickkan2004@gmail.com>
2023-03-23 20:30:05 -0300

    main

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ console.log(div)
 // const input = document.querySelector('input')
 
 // input.addEventListener('keyup', soltou)
-// Lista simples - adicionar
+// Lista simples - adicionar itens escritos no input
 const input = document.querySelector('input')
 const lista = document.querySelector('ul')
 const item = document.querySelector('li')
